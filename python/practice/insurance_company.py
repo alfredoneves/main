6e6abafce2e9885c98d872ba5a858bffabf3522c
alfredoneves <alfredoncdossantos@gmail.com>
2022-10-21 14:40:57 -0300

    insurance_company.py

---
+++ python/practice/insurance_company.py
@@ -0,0 +1,8 @@
+
+name = input("enter the company name: ")
+employees = int(input("enter the number of employees: "))
+location = input("enter the location of the company (city): ")
+lowest = float(input("enter the lowest price for a policy: "))
+highest = float(input("enter the highest price for a policy: "))
+print(f"We are Company {name} located in {location}. Our {employees} employees can help you find the policy that is right for you with policies ranging from ${lowest} to ${highest} per month!")
+
