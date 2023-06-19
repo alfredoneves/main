9249ded92ae351b659c9acd5e19ee293b61b682b
horus <alfredoncdossantos@gmail.com>
2023-06-19 15:24:09 -0300

    statistics intro

---
+++ python/practice/statistics_intro.py
@@ -0,0 +1,7 @@
+import statistics
+
+my_list = [47, 95, 88, 73, 88, 84]
+print(f'list: {my_list}')
+print(f'mean: {statistics.mean(my_list)}')
+print(f'median: {statistics.median(my_list)}')
+print(f'mode: {statistics.mode(my_list)}')
