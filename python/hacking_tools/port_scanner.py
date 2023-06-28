4c4b7a52eee01921dc08c136724f76ab7fc07c94
horus <alfredoncdossantos@gmail.com>
2023-06-27 21:36:42 -0300

    port scanner refactored

--- python/hacking_tools/port_scanner.py
+++ python/hacking_tools/port_scanner.py
@@ -2,55 +2,80 @@
 
 import socket
 import threading
-from queue import Queue
 import sys
+from queue import Queue
+from datetime import datetime
 
-try:
-	target = sys.argv[1]
-	queue = Queue()
-	# take the range of ports to scan
-	port_range = sys.argv[2]
-	delimiter = port_range.find("-")
-	first = int(port_range[0:delimiter])
-	last = int(port_range[delimiter+1::])
-	port_list = list(range(first, last))	# list of ports that will be used to scan
-	# save open ports
-	open_ports = []
-except:
-	print("usage: python3 port_scanner2.py [ip] [port_range]. EXP: python3 port_scanner2.py 192.168.0.1 1-1024")
 
-def port_scan(port):
+def port_scan(host, port) -> bool:
+	"""
+	This function tries to connect to a host and port.
+	:param host: IP address of target
+	:param port: port to connect to
+	:return: True if the connection succeeds and False in case of error
+	"""
 	try:
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect((target, port))
+		s.connect((host, port))
+		s.settimeout(1.5)
 		s.close()
 		return True
 	except:
 		return False
-	
-def fill_queue(port_list):
-	for port in port_list:
-		queue.put(port)
-		
+
+
+def fill_queue(ports: list, my_queue: Queue):
+	"""
+	This function receives a list and a queue and fills the queue with the list.
+	:param ports: list of ports
+	:param my_queue: queue object
+	"""
+	for port in ports:
+		my_queue.put(port)
+
+
 def attack():
+	global host
+	global queue
+
 	while not queue.empty():
-		port = queue.get()
-		if port_scan(port):
+		port: int = queue.get()
+		if port_scan(host, port):
 			print(f"port {port} is open!")
-			open_ports.append(port)
 
-fill_queue(port_list)	# fills the list of ports to scan
+
+def start():
+	"""
+	This function takes the host and the number of threads
+	:return host, threads
+	"""
+	try:
+		host = str(sys.argv[1])
+		threads = int(sys.argv[2])
+		return host, threads
+	except:
+		print("usage: python3 port_scanner.py [host] [threads] \nEXP: python3 port_scanner.py nmap.org 300")
+		sys.exit()
+
+
+def main(function):
+	for i in range(threads):
+		thread = threading.Thread(target=function)  # tells the thread what function to execute
+		thread_list.append(thread)
+
+	# The following 2 for need to be separate, otherwise only 1 thread will be launched in the beginning
+	for thread in thread_list:
+		thread.start()
+
+	for thread in thread_list:
+		thread.join()  # waits for all threads to finish
+
+
+queue = Queue()
+fill_queue([i for i in range(1, 65535)], queue)
 thread_list = []
+host, threads = start()
 
-for i in range(16):
-	thread = threading.Thread(target=attack)	# tells the thread what function to execute
-	thread_list.append(thread)
-	
-# the following 2 "fors" need to be separate, otherwise only 1 thread will be launched in the beggining
-for thread in thread_list:
-	thread.start()
-	
-for thread in thread_list:
-	thread.join()	# waits for all threads to finish
-print(f"open ports: {open_ports}")
-
+if __name__ == '__main__':
+	main(attack)
+	print(f'Scan finished at: {datetime.utcnow()}')
