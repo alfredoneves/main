192126aa2f23ec4732baacf8b5de75467346b10a
horus <alfredoncdossantos@gmail.com>
2023-08-18 22:48:33 -0300

    zone transfer script updated

--- bash/zone_transfer.sh
+++ bash/zone_transfer.sh
@@ -2,5 +2,5 @@
 
 target=$1
 name_servers=$(host -t ns $target | cut -d " " -f 4)
-for server in $name_servers; do host -l -a $server; done
+for server in $name_servers; do dig @$server $target axfr ; done
 
