12c3ea0d830f6a11c290d2eb3662484148cbae62
alfredoneves <106417859+alfredoneves@users.noreply.github.com>
2026-02-06 19:03:04 -0300

    Update 1-calculator.c

--- c/freeCodeCamp/1-calculator.c
+++ c/freeCodeCamp/1-calculator.c
@@ -1,17 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void){
-	double first;
-	double second;
+int main(){
+  char buffer[10];
+  char operator[3];
+  int num1, num2;
 
-	printf("Calculator started!\n");
-	printf("Enter the first number: ");
-	scanf("%lf", &first);
+  printf("Calculator started!\n");
 
-	printf("Enter the seconds number: ");
-	scanf("%lf", &second);
+  printf("Type the operation (+, - , *, /): ");
+  fgets(operator, sizeof(operator), stdin);
+  operator[strcspn(operator, "\n")] = '\0';
 
-	printf("%lf + %lf = %lf", first, second, (first+second));
+  printf("Type the first number: ");
+  fgets(buffer, sizeof(buffer), stdin);
+  num1 = atoi(buffer);
 
-	return 0;
+  printf("Type the second number: ");
+  fgets(buffer, sizeof(buffer), stdin);
+  num2 = atoi(buffer);
+
+  printf("%i %s %i = ", num1, operator, num2);
+
+  if(strcmp(operator, "+") == 0){
+        printf("%i\n", num1 + num2);
+  } else if(strcmp(operator, "-") == 0){
+        printf("%i\n", num1 - num2);
+  } else if(strcmp(operator, "*") == 0){
+        printf("%i\n", num1 * num2);
+  } else {
+        if(num2 == 0){
+                printf("Division by 0 is not permitted!\n");
+        } else {
+                printf("%i\n", num1 / num2);
+        }
+  }
+
+  printf("Calculator finished!\n");
+
+  return 0;
 }
